5a2f30fa379f95f7eb932d719992a4af11583be3
Shiwang-Srivastava <r.shiw019@gmail.com>
2026-03-19 18:19:06 +0530

    fix(db): add certifi for ssl verification

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+import certifi
 
-MONGO_DETAILS = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_DETAILS = os.getenv("MONGO_URL", "mongodb+srv://shiwang019_db_user:wgf0lbPmXBrr9XCr@cluster0.sganlkl.mongodb.net/?appName=Cluster0")
 
-client = AsyncIOMotorClient(MONGO_DETAILS)
+ca = certifi.where()
+client = AsyncIOMotorClient(MONGO_DETAILS, tlsCAFile=ca)
 
 database = client.hrms_lite
 
